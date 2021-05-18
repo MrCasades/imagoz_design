438b8cc924b388561c59d278e8488705b63d44b5
MrCasades <casades@list.ru>
2021-05-19 00:19:48 +0300

    new main menu

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 // Главное меню
 
-const nav = document.getElementById('nav')
+// const nav = document.getElementById('nav')
 
-nav.addEventListener('click', function(e) {
-    const target = e.target;
+// nav.addEventListener('click', function(e) {
+//     const target = e.target;
 
-    const targetParent = target.closest('.menu-item')
+//     const targetParent = target.closest('.menu-item')
 
-    if (targetParent) {
-        const subm = targetParent.getElementsByClassName('submenu')[0]
-        close()
+//     if (targetParent) {
+//         const subm = targetParent.getElementsByClassName('submenu')[0]
+//         close()
 
-        if (subm) {
-            subm.style.display = 'block';
-        }
-  }
-});
+//         if (subm) {
+//             subm.style.display = 'block';
+//         }
+//   }
+// });
 
-function close() {
-    const s = document.getElementsByClassName('submenu')
+// function close() {
+//     const s = document.getElementsByClassName('submenu')
     
-    for (var i = 0; i < s.length; i++) {
-        s[i].style.display = 'none';
-  }
-}
+//     for (var i = 0; i < s.length; i++) {
+//         s[i].style.display = 'none';
+//   }
+// }
 
 $(document).ready(function() {
+    "use strict";
+    $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+    $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+    $(".menu > ul").before("<span class=\"menu-mobile\">МЕНЮ:</span>");
+    $(".menu > ul > li").hover(function(e) {
+        if ($(window).width() > 768) {
+            $(this).children("ul").stop(true, false).fadeToggle(150);
+            e.preventDefault();
+        }
+    });
+    $(".menu > ul > li").click(function() {
+        if ($(window).width() <= 768) {
+            $(this).children("ul").fadeToggle(150);
+        }
+    });
+    $(".menu-mobile").click(function(e) {
+        $(".menu > ul").toggleClass('show-on-mobile');
+        e.preventDefault();
+    });
 
     //Форма авторизации
 
